efea5adcba2934748867bf0fca47a635df2d78fe
Jonathan Krall <jonathan.krall@lexicon.se>
2024-04-10 11:59:00 +0200

    Scaffolded Employee-Controller. Added some database. Created EmployeeIndexViewModel

--- MVCIntro/Models/Employee.cs
+++ MVCIntro/Models/Employee.cs
@@ -1,8 +1,13 @@
-﻿namespace MVCIntro.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCIntro.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
+
+        public string Department { get; set; }
     }
 }
